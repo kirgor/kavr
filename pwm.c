1b221641ea6524978034cbce1313df06bd02514c
Kirill Gorobets <kgorob@gmail.com>
2018-01-10 02:10:10 +0300

    PWM on timer 1 supported

--- pwm.c
+++ pwm.c
@@ -6,8 +6,14 @@
 
 void pwm_0_init(uint8_t prescaler_mask) {
 	port_d_output_mode(5, 6);
-	set_flags(TCCR0A, COM0A1, COM0B1, WGM00/*, WGM01*/);
+	set_flags(TCCR0A, COM0A1, COM0B1, WGM00);
 	TCCR0B |= prescaler_mask;
+}
+
+void pwm_1_init(uint8_t prescaler_mask) {
+	port_b_output_mode(1, 2);
+	set_flags(TCCR1A, COM1A1, COM1B1, WGM10);
+	TCCR1B |= prescaler_mask;
 }
 
 #endif
--- pwm.h
+++ pwm.h
@@ -11,9 +11,13 @@
 #define PWM_PRESCALER_256	4
 #define PWM_PRESCALER_1024	5
 
+#define pwm_pb1_write(value) OCR1A=value
+#define pwm_pb2_write(value) OCR1B=value
 #define pwm_pd5_write(value) OCR0B=value
 #define pwm_pd6_write(value) OCR0A=value
 
 void pwm_0_init(uint8_t prescaler_mask);
+
+void pwm_1_init(uint8_t prescaler_mask);
 
 #endif
